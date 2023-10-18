97a65ef3373aefc4ae6eb54f81459d6f92467bae
Joe <darckProyect8@gmail.com>
2023-10-18 01:47:03 -0500

    Dashboard graphics

---
+++ src/helpers/dataGraphics.ts
@@ -0,0 +1,101 @@
+export const dataSimpleBarChart = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
+export const dataScatterAndLineOfBestFit = [
+  { index: 10000, red: 1643, blue: 790 },
+  { index: 1666, red: 182, blue: 42 },
+  { index: 625, red: 56, blue: 11 },
+  // Calculation of line of best fit is not included in this demo
+  { index: 300, redLine: 0 },
+  { index: 10000, redLine: 1522 },
+  { index: 600, blueLine: 0 },
+  { index: 10000, blueLine: 678 },
+];
+
+export const dataPieChartWithCustomizedLabel = [
+  { name: "Group A", value: 400 },
+  { name: "Group B", value: 300 },
+  { name: "Group C", value: 300 },
+  { name: "Group D", value: 200 },
+];
+
+export const dataSpecifiedDomainRadarChart = [
+  {
+    subject: "Math",
+    A: 120,
+    B: 110,
+    fullMark: 150,
+  },
+  {
+    subject: "Chinese",
+    A: 98,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: "English",
+    A: 86,
+    B: 130,
+    fullMark: 150,
+  },
+  {
+    subject: "Geography",
+    A: 99,
+    B: 100,
+    fullMark: 150,
+  },
+  {
+    subject: "Physics",
+    A: 85,
+    B: 90,
+    fullMark: 150,
+  },
+  {
+    subject: "History",
+    A: 65,
+    B: 85,
+    fullMark: 150,
+  },
+];
--- src/pages/admin/Dashboard/Dashboard.tsx
+++ src/pages/admin/Dashboard/Dashboard.tsx
@@ -1,5 +1,234 @@
+import {
+  dataPieChartWithCustomizedLabel,
+  dataScatterAndLineOfBestFit,
+  dataSimpleBarChart,
+  dataSpecifiedDomainRadarChart,
+} from "@/helpers/dataGraphics";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  ComposedChart,
+  Legend,
+  Line,
+  Pie,
+  PieChart,
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
+  Rectangle,
+  Scatter,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+type renderTypeCustom = {
+  cx: number;
+  cy: number;
+  midAngle: number;
+  innerRadius: number;
+  outerRadius: number;
+  percent: number;
+  index: number;
+};
+
+const RADIAN = Math.PI / 180;
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}: renderTypeCustom) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
 const Dashboard = () => {
-  return <div>Dashboard </div>;
+  return (
+    <div>
+      <h2 className="text-4xl font-bold">Dashboard</h2>
+
+      <div className="grid grid-cols-2">
+        {/* Grafico 1 */}
+        <div>
+          <BarChart
+            width={500}
+            height={300}
+            data={dataSimpleBarChart}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray={"3 3"} />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar
+              dataKey="pv"
+              fill="#8884d8"
+              activeBar={
+                <Rectangle
+                  fill="pink"
+                  stroke="blue"
+                />
+              }
+            />
+            <Bar
+              dataKey="uv"
+              fill="#82ca9d"
+              activeBar={
+                <Rectangle
+                  fill="gold"
+                  stroke="purple"
+                />
+              }
+            />
+          </BarChart>
+        </div>
+        {/* Grafico 2 */}
+        <div>
+          <ComposedChart
+            width={500}
+            height={400}
+            data={dataScatterAndLineOfBestFit}
+            margin={{
+              top: 20,
+              right: 80,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid stroke="#f5f5f5" />
+            <Tooltip />
+            <Legend />
+
+            <XAxis
+              dataKey="index"
+              type="number"
+              label={{
+                value: "Index",
+                position: "insideBottomRight",
+                offset: 0,
+              }}
+            />
+            <YAxis
+              unit="ms"
+              type="number"
+              label={{ value: "Time", angle: -90, position: "insideLeft" }}
+            />
+            <Scatter
+              name="red"
+              dataKey="red"
+              fill="red"
+            />
+            <Scatter
+              name="blue"
+              dataKey="blue"
+              fill="blue"
+            />
+            <Line
+              dataKey="blueLine"
+              stroke="blue"
+              dot={false}
+              activeDot={false}
+              legendType="none"
+            />
+            <Line
+              dataKey="redLine"
+              stroke="red"
+              dot={false}
+              activeDot={false}
+              legendType="none"
+            />
+          </ComposedChart>
+        </div>
+        {/* Grafico 3 */}
+        <div>
+          <PieChart
+            width={400}
+            height={400}
+          >
+            <Pie
+              data={dataPieChartWithCustomizedLabel}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
+              outerRadius={80}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {dataPieChartWithCustomizedLabel.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </div>
+        {/* Grafico 4 */}
+        <div>
+          <RadarChart
+            cx={300}
+            cy={250}
+            outerRadius={150}
+            width={500}
+            height={500}
+            data={dataSpecifiedDomainRadarChart}
+          >
+            <PolarGrid />
+            <PolarAngleAxis dataKey="subject" />
+            <PolarRadiusAxis
+              angle={30}
+              domain={[0, 150]}
+            />
+            <Radar
+              name="Mike"
+              dataKey="A"
+              stroke="#8884d8"
+              fill="#8884d8"
+              fillOpacity={0.6}
+            />
+            <Radar
+              name="Lily"
+              dataKey="B"
+              stroke="#82ca9d"
+              fill="#82ca9d"
+              fillOpacity={0.6}
+            />
+            <Legend />
+          </RadarChart>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
